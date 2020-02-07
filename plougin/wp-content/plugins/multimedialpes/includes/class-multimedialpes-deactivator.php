99641ffe4e2994de65d09237333175034f841fa8
leoboyerbx <leo.boyer@outlook.com>
2020-02-07 15:32:14 +0100

    Loaded new plugin boilerplate in place

---
+++ plougin/wp-content/plugins/multimedialpes/includes/class-multimedialpes-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       contact@leoboyer.fr
+ * @since      1.0.0
+ *
+ * @package    Multimedialpes
+ * @subpackage Multimedialpes/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Multimedialpes
+ * @subpackage Multimedialpes/includes
+ * @author     Léo Boyer <contact@leoboyer.fr>
+ */
+class Multimedialpes_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ plougin/wp-content/plugins/multimedialpes/includes/class-multimedialpes-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       contact@leoboyer.fr
+ * @since      1.0.0
+ *
+ * @package    Multimedialpes
+ * @subpackage Multimedialpes/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Multimedialpes
+ * @subpackage Multimedialpes/includes
+ * @author     Léo Boyer <contact@leoboyer.fr>
+ */
+class Multimedialpes_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'multimedialpes',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
