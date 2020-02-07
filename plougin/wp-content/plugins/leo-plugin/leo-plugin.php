b0fcb7cf73952e17cf5be6baa773e78d36adfd5d
leoboyerbx <leo.boyer@outlook.com>
2020-02-07 08:40:56 +0100

    update gitignore

---
+++ plougin/wp-content/plugins/leo-plugin/leo-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Leo plugin
+Plugin URI: http://leoboyer.fr/web/qwwizzy
+Description: Un plugin d'introduction pour le développement sous WordPress
+Version: 0.1
+Author: Léo léo
+Author URI: http://leoboyer.fr
+License: GPL2
+*/
