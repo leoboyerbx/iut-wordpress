383e5c1bcdb30e1ecfd99affd0baa37533734718
leoboyerbx <leo.boyer@outlook.com>
2020-03-10 17:04:38 +0100

    Cartes du shortcode dynamiques partie 1

--- plougin/wp-content/plugins/multimedialpes/public/partials/multimedialpes-public-display.php
+++ plougin/wp-content/plugins/multimedialpes/public/partials/multimedialpes-public-display.php
@@ -29,21 +29,22 @@ function multimedialpes_public_display ($atts, $content) {
     if ($candidates_data->have_posts()):
         echo '<div class="multimedialpes-candidatures">';
     while ($candidates_data->have_posts()): $candidates_data->the_post();
+        $type = get_post_meta( get_the_ID(), 'candidat_type', true );
         ?>
         <figure class="multimedialpes_card">
             <div class="multimedialpes_card__hero">
-                <img src="https://images.unsplash.com/photo-1474600056930-615c3d706456?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" alt="multimedialpes_card" class="multimedialpes_card__img">
+                <img src="<?= get_post_meta( get_the_ID(), 'candidat_thumbnail', true ) ?>" alt="multimedialpes_card" class="multimedialpes_card__img">
             </div>
             <div class="multimedialpes_card__content">
                 <div class="multimedialpes_card__title">
-                    <h1 class="multimedialpes_card__heading">multimedialpes_card Vegetale 👌</h1>
-                    <div class="multimedialpes_card__tag multimedialpes_card__tag--1">#vegetarian</div>                   <div class="multimedialpes_card__tag multimedialpes_card__tag--2">#italian</div>
+                    <h1 class="multimedialpes_card__heading"><?= the_title(); ?></h1>
+                </div>
+                <div class="multimedialpes_card__tags">
+                    <div class="multimedialpes_card__tag multimedialpes_card__tag--<?= strtolower($type); ?>"><?= $type ?></div>
                 </div>
                 <p class="multimedialpes_card__description">Yummy veggie multimedialpes_card with tasty olives, crisp peppers, fresh arugula and original italian tomato sauce.</p>
                 <div class="multimedialpes_card__details">
                     <div class="multimedialpes_card__detail"><span class="emoji">🍕</span>850 kcal</div>
-                    <div class="multimedialpes_card__detail"><span class="emoji">⏱</span>30 min</div>
-                    <div class="multimedialpes_card__detail"><span class="emoji">⭐</span>4.7 / 5</div>
                 </div>
             </div>
         </figure>
